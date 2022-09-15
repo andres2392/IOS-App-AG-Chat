dbb0fa6b5022b752224868704d69efd8c54117df
Andres <andres.gonzalez2392@gmail.com>
2022-09-15 12:56:16 -0400

    Initial Commit

---
+++ Shared/AGChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGChatApp.swift
+//  Shared
+//
+//  Created by PDWS on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AGChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
